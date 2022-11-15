571b12bd3679b9ed69247d741e7644b6da310f37
Mariah Talicuran <mariah.talicuran@aa.com>
2022-11-15 10:14:22 -0700

    added word list and added hooks for state mngmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import {useState} from 'react';
+import words from "./wordList.json"
 import './App.css';
 
 function App() {
+const [wordToGuess, setWordToGuess] = useState(() => {
+  return words[Math.floor(Math.random() * words.length)]
+});
+const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+console.log(wordToGuess)
+;
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Let's make hangman!
-        </p>
-        
-      </header>
-    </div>
-  );
-}
+    <>
+     <h1>Hello</h1>
+  
+    </>
+     );
+};
+ 
+ 
 
 export default App;
